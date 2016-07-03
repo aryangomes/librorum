0b63f41797508cebb42405885417c633ee96dc9c
Luan Martins <luan_18martins@hotmail.com>
2016-07-02 22:41:32 -0300

    modificacoes no modelo e _form

--- yii2/views/usuario/_form.php
+++ yii2/views/usuario/_form.php
@@ -1,7 +1,8 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use \yii\widgets\MaskedInput;
+use kartik\form\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Usuario */
@@ -12,11 +13,24 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'nome', [
+    'feedbackIcon' => [
+        //'prefix' => 'fa fa-',
+        'default' => 'user',
+        'success' => 'user-plus',
+        'error' => 'user-times',
+        'defaultOptions' => ['class'=>'text-warning']
+    ]
+    ])->textInput(['placeholder'=>'Enter username...']);
 
-    <?= $form->field($model, 'rg')->textInput(['maxlength' => true]) ?>
+?>
+    <?= $form->field($model, 'rg')->widget(MaskedInput::className(), [
+        'mask' => ['99.999.999.9'],
+    ]);?>
 
-    <?= $form->field($model, 'cpf')->textInput(['maxlength' => true]) ?>
+    <?=  $form->field($model, 'cpf')->widget(MaskedInput::className(), [
+        'mask' => ['999.999.999-99'],
+    ]); ?>
 
     <?= $form->field($model, 'cargo')->textInput(['maxlength' => true]) ?>
 
@@ -24,9 +38,26 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'endereco')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'telefone')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'telefone', [
+    'feedbackIcon' => [
+        //'prefix' => 'fa fa-',
+        'default' => 'phone',
+        'success' => 'check-circle',
+        'error' => 'exclamation-circle',
+    ]
+])->widget(MaskedInput::className(), [
+    'mask' => '(99)99999-9999'
+]);?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'email', [
+    'feedbackIcon' => [
+        'default' => 'envelope',
+        'success' => 'ok',
+        'error' => 'exclamation-sign',
+        'defaultOptions' => ['class'=>'text-primary']
+    ]
+])->textInput(['placeholder'=>yii::t('app','Enter a valid email address...')]);
+ ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
