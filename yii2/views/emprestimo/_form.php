589baf71742a98d32851608c4c4625af3020e970
Aryan <aryan.gomes@gmail.com>
2016-07-06 20:11:33 -0300

    Formulário de Empréstimo formatado com FormBuilder

--- yii2/views/emprestimo/_form.php
+++ yii2/views/emprestimo/_form.php
@@ -1,9 +1,10 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use kartik\widgets\ActiveForm;
 use yii\bootstrap\Modal;
-
+use kartik\builder\Form;
+use kartik\builder\FormGrid;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Emprestimos */
@@ -14,95 +15,202 @@ use yii\bootstrap\Modal;
 
 <div class="emprestimos-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php
+    $form = ActiveForm::begin(['type' => ActiveForm::TYPE_VERTICAL]);
+
+    echo FormGrid::widget([
+        'model' => $model,
+        'form' => $form,
+        'autoGenerateColumns' => true,
+        'rows' => [
+            [
+                'contentBefore' => '<legend class="text-info"><small>Usuário</small></legend>',
+                'attributes' => [
+
+                    'usuario_rg' => ['type' => Form::INPUT_WIDGET,'widgetClass' => '\yii\widgets\MaskedInput', 'options' =>
+                        [ 'mask' => ['99.999.999-9']]
+                    ],
+                    'usuario_nome' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['disabled' => true]
+                    ],
 
 
-    <?= $form->field($model, 'usuario_rg')->textInput(['placeholder' => 'Digite RG do Usuário']) ?>
+                ],
 
-    <?= $form->field($model, 'usuario_nome')->textInput(['disabled' => true]) ?>
+            ],
+
+
+        ]
+    ]);
+    ?>
+
+    <!--   Busca de Usuário     -->
+    <?php
+    Modal::begin([
+        'header' => '<h2>Buscar Usuário</h2>',
+        'toggleButton' => ['label' => 'Buscar Usuário', 'class' => 'btn btn-primary btn-block',],
+
+    ]);
+
+    ?>
+    <div class="form-group">
+        <?= Html::label('Nome do usuário', 'busca-usuario') ?>
+
+        <?= Html::input('text', 'busca-usuario', null, ['class' => 'form-control', 'id' => 'busca-usuario',
+            'placeholder' => 'Digite o nome do usuário']) ?>
+
+    </div>
+    <div id="result-messagem-busca-usuario">
+    </div>
+    <table id="tableresult" class="table table-bordered">
+        <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Matrícula</th>
+            <th>Ações</th>
+        </tr>
+        </thead>
+        <tbody id="tbody-result">
+
+        </tbody>
+    </table>
+
+
+    <?php
+
+
+    Modal::end();
+    ?>
+    <!--    Busca de Usuário      -->
+
+    <?= FormGrid::widget([
+        'model' => $usuario,
+        'form' => $form,
+        'autoGenerateColumns' => true,
+        'rows' => [
+
+            [
+                'contentBefore' => '<legend class="text-info"><small>Informações do Usuário</small></legend>',
+                'attributes' => [
+
+                    'cpf' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['disabled' => true]
+                    ],
+                    'cargo' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['disabled' => true]
+                    ],
+                    'reparticao' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['disabled' => true]
+                    ],
+
+                ]
+            ],
+
+        ]
+    ]);
+    ?>
+
+    <?= FormGrid::widget([
+        'model' => $exemplar,
+        'form' => $form,
+        'autoGenerateColumns' => true,
+        'rows' => [
+
+            [
+                'contentBefore' => '<legend class="text-info"><small>Código Exemplar</small></legend>',
+                'attributes' => [
+
+                    'codigo_livro' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['placeholder' => 'Digite o código do exemplar']
+                    ],
+
+
+                ]
+            ],
+
+        ]
+    ]);
+    ?>
+    <div id="message-indisponivel-exemaplar">
+
+
+    </div>
+    <?= FormGrid::widget([
+        'model' => $acervo,
+        'form' => $form,
+        'autoGenerateColumns' => true,
+        'rows' => [
+
+            [
+                'contentBefore' => '<legend class="text-info"><small>Informações do Exemplar</small></legend>',
+                'attributes' => [
+
+                    'titulo' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['disabled' => true]
+                    ],
+
+                    'autor' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['disabled' => true]
+                    ],
+
+                ]
+            ],
+
+        ]
+    ]);
+    ?>
+
+    <?= FormGrid::widget([
+        'model' => $model,
+        'form' => $form,
+        'autoGenerateColumns' => true,
+        'rows' => [
+
+            [
+                'contentBefore' => '<legend class="text-info"><small>Informações sobre o Empréstimo</small></legend>',
+                'attributes' => [
+
+                    'dataemprestimo' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['disabled' => true,
+                            'value' => date('d/m/Y H:i:s', strtotime($model->dataemprestimo))]
+                    ],
+
+                    'dataprevisaodevolucao' => ['type' => Form::INPUT_TEXT, 'options' =>
+                        ['disabled' => true,"id" => "lb-dataprevisaodevolucao",
+                        ]
+                    ],
+
+                ]
+            ],
+
+        ]
+    ]);
+    ?>
+
 
     <?= $form->field($model, 'usuario_idusuario')->hiddenInput()->label(false) ?>
 
+    <?= $form->field($model, 'usuario_rg')->hiddenInput(['id' => 'rgusuario'])->label(false) ?>
+
     <?= $form->field($model, 'usuario_nome')->hiddenInput(['id' => 'nomeusuario'])->label(false) ?>
-
-    <?= $form->field($usuario, 'cpf')->textInput(['disabled' => true]) ?>
-
-    <?= $form->field($usuario, 'cargo')->textInput(['disabled' => true]) ?>
-
-    <?= $form->field($usuario, 'reparticao')->textInput(['disabled' => true]) ?>
-
-    <?= $form->field($exemplar, 'codigo_livro')->textInput(
-        ['placeholder' => 'Digite o código do exemplar']
-    ) ?>
-
 
     <?= $form->field($model, 'acervo_exemplar_idacervo_exemplar')->hiddenInput()->label(false) ?>
 
     <div id="message-indisponivel-exemaplar">
-        
+
 
     </div>
 
-    <?= $form->field($acervo, 'titulo')->textInput(['disabled' => true]) ?>
+    <?= $form->field($model, 'dataprevisaodevolucao')->hiddenInput()->label(false) ?>
 
-    <?= $form->field($acervo, 'autor')->textInput(['disabled' => true]) ?>
 
-    <?= $form->field($model, 'dataemprestimo')->textInput(['disabled' => true,
-        'value' => date('d/m/Y H:i:s', strtotime($model->dataemprestimo))]) ?>
 
-    <div class="form-group">
-        <?= $form->field($model, 'dataprevisaodevolucao')->hiddenInput()->label(false) ?>
-
-        <?= Html::label("Data de previsão da devolução") ?>
-
-        <?= Html::input("text", null, null, ["id" => "lb-dataprevisaodevolucao", 'class' => "form-control",
-            'disabled' => true]) ?>
-    </div>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'),
             ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary',
-            'id'=>'btCreate']) ?>
-
-        <!--   Busca de Usuário     -->
-        <?php
-        Modal::begin([
-            'header' => '<h2>Buscar Usuário</h2>',
-            'toggleButton' => ['label' => 'Buscar Usuário', 'class' => 'btn btn-primary',],
-
-        ]);
-
-        $form = ActiveForm::begin();
-
-        ?>
-        <div class="form-group">
-            <?= Html::label('Nome do usuário', 'busca-usuario') ?>
-
-            <?= Html::input('text', 'busca-usuario', null, ['class' => 'form-control', 'id' => 'busca-usuario',
-                'placeholder' => 'Digite o nome do usuário']) ?>
-
-        </div>
-        <div id="result-messagem-busca-usuario">
-        </div>
-        <table id="tableresult" class="table table-bordered">
-            <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Matrícula</th>
-                <th>Ações</th>
-            </tr>
-            </thead>
-            <tbody id="tbody-result">
-
-            </tbody>
-        </table>
+                'id'=>'btCreate']) ?>
 
 
-        <?php
-        ActiveForm::end();
-
-        Modal::end();
-        ?>
-        <!--    Busca de Usuário      -->
         <?= Html::resetButton('Limpar',
             ['class' => 'btn btn-warning']) ?>
     </div>
--- yii2/web/js/js-emprestimo-form.js
+++ yii2/web/js/js-emprestimo-form.js
@@ -5,7 +5,7 @@
 
 $('#tableresult').hide();
 
-$('#emprestimo-usuario_rg').keyup(function () { 
+$('#emprestimo-usuario_rg').blur(function () {
     var
         rg = $(this).val();
     if (rg != ' ') {
@@ -17,7 +17,7 @@ $('#emprestimo-usuario_rg').keyup(function () {
                 data = $.parseJSON(data);
 
             if (data != null) {
-
+                $('#rgusuario').val(rg);
                 $('#usuario-cpf').val(data.cpf);
                 $('#emprestimo-usuario_nome').val(data.nome);
                 $('#nomeusuario').val(data.nome);
@@ -30,32 +30,8 @@ $('#emprestimo-usuario_rg').keyup(function () {
     }
 });
 
-$('#emprestimo-usuario_rg').change(function () {
-    var
-        rg = $(this).val();
-    if (rg != ' ') {
 
-
-        $.get('get-usuario', {rg: rg}, function (data) {
-
-            var
-                data = $.parseJSON(data);
-
-            if (data != null) {
-
-                $('#usuario-cpf').val(data.cpf);
-                $('#emprestimo-usuario_nome').val(data.nome);
-                $('#nomeusuario').val(data.nome);
-                $('#usuario-cargo').val(data.cargo);
-                $('#usuario-reparticao').val(data.reparticao);
-                $('#emprestimo-usuario_idusuario').val(data.idusuario);
-
-            }
-        });
-    }
-});
-
-$('#acervoexemplar-codigo_livro').keyup(function () {
+$('#acervoexemplar-codigo_livro').blur(function () {
     var
         codigoExemplar = $(this).val();
     if (codigoExemplar != ' ') {
@@ -86,41 +62,6 @@ $('#acervoexemplar-codigo_livro').keyup(function () {
     }
 });
 
-
-$('#acervoexemplar-codigo_livro').change(function () {
-    var
-        codigoExemplar = $(this).val();
-    if (codigoExemplar != ' ') {
-
-
-        $.get('get-exemplar', {codigoExemplar: codigoExemplar}, function (data) {
-
-
-            var
-                data = $.parseJSON((data));
-            if (data != null) {
-
-                $('#acervo-titulo').val(data[1].titulo);
-                $('#acervo-autor').val(data[1].autor);
-                $('#emprestimo-acervo_exemplar_idacervo_exemplar').val(data[0].idacervo_exemplar);
-                $("#message-indisponivel-exemaplar").html("");
-                if (!(data[0].esta_disponivel)) {
-                    $("#message-indisponivel-exemaplar").html("<div class=\"alert alert-warning\" role=\"alert\">"+
-                        "<strong>Alerta!</strong> Exemplar indisponível no momento."+
-                        "</div>");
-                    $('button[type="submit"]').prop('disabled',true);
-
-                }else{
-                    $("#message-indisponivel-exemaplar").html("");
-                    $('button[type="submit"]').prop('disabled',false);
-                }
-
-            }
-
-
-        });
-    }
-});
 $.get('get-data-previsao-devolucao', function (data) {
     console.log('previsao.: ' + data);
 
@@ -132,7 +73,7 @@ $.get('get-data-previsao-devolucao', function (data) {
 
 });
 
-$('#busca-usuario').keyup(function () {
+$('#busca-usuario').blur(function () {
     var
         buscausuario = $(this).val();
 
@@ -169,43 +110,6 @@ $('#busca-usuario').keyup(function () {
                     $('#usuario-departamento_iddepartamento').val(usuario[3].nome_departamento);
 
                 });
-            } else {
-                data = null;
-                $('#tableresult').hide();
-                $('#tbody-result').html('');
-                $('#result-messagem-busca-usuario').attr('class', 'alert alert-danger');
-                $('#result-messagem-busca-usuario').html('Nenhuma matrícula encontrada');
-            }
-        });
-    }
-});
-
-$('#busca-usuario').change(function () {
-
-    var
-        buscausuario = $(this).val();
-
-    if (buscausuario != ' ') {
-
-
-        $.get('get-busca-usuario', {nomeUsuario: buscausuario}, function (data) {
-
-            var
-                data = $.parseJSON(data);
-
-            if (data.length > 0) {
-                $('#tableresult').show();
-                $('#tbody-result').html('');
-
-                $('#usuario-rg').val(data.rg);
-                $('#result-messagem-busca-usuario').attr('class', 'alert alert-success');
-                $('#result-messagem-busca-usuario').html('</b>Matrículas encontradas</b>');
-                data.forEach(function (item) {
-                    $('#tbody-result').append(
-                        '<tr><td>' + item.nome + '</td><td>' + item.rg + '</td><td><a href=\'#\' onclick=\'actionSelecionarUsuario(' + item.rg + ')\' class=\'btn btn-success\' id=\'actionbuscar\' > <span class="glyphicon glyphicon-ok"></span></a ></td></tr>');
-                });
-
-
             } else {
                 data = null;
                 $('#tableresult').hide();
