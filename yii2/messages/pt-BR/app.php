a31d830f95b3d4ecfbe2192293ceaf5bc6965873
Luan Martins <luan_18martins@hotmail.com>
2016-08-05 23:32:22 -0300

    modificacoes de internacionalizaçao

--- yii2/messages/pt-BR/app.php
+++ yii2/messages/pt-BR/app.php
@@ -31,5 +31,10 @@
 		'User' => 'Usuário',
 		'Users' => 'Usuários',
 		'situacaoUsuarioIdsituacaoUsuario' => 'Situação Do Usuário',
+		'Idsituacao Usuario' => 'Id Situação Usuário',
+		'Situacao' => 'Situação',
+		'Situacao Usuarios' => 'Situação Dos Usuários',
+		'Idtipo Aquisicao' => 'Id Tipo Aquisição'
+		
 	];
 ?>
